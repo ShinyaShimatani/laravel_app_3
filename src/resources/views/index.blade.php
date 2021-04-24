a18807cd2435e7f6e7c1632c47a178a71827c8cf
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-04-24 11:08:05 +0900

    modify

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,83 +1,57 @@
-<?php
-$connect = new PDO("mysql:host=mysql;dbname=default;charset=utf8;",  'default',  'secret' );
-if(!$connect){
-    echo "データベースに接続できません";
-}
-$sql="SELECT id, name, mimic, maso, pathos, updown, black, volume, energy, insane, color FROM members ORDER BY id";   
-$stmt = $connect->prepare($sql);
-$stmt->execute(); 
-$data= "";
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    $data.= "{";
-    $data.= "    label: '".$row["name"]."',";
-    $data.= "    data: [".$row["mimic"].", ".$row["maso"].", ".$row["mimic"].", ".$row["pathos"].", ";
-    $data.= $row["updown"].", ".$row["black"].", ".$row["volume"].", ".$row["energy"].", ".$row["insane"]."],";
-    $data.= "    backgroundColor: 'rgba(".$row["color"].",0.4)',";
-    $data.= "   borderColor: 'rgba(123, 255, 0, 1)',";
-    $data.= "   borderWidth: 1";
-    $data.= "},";
-}
-//echo $data;
-$shurui="radar";
-if( !empty($_POST["shurui"]) ){ $shurui=$_POST["shurui"]; }
-?>
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Chart.jsでモダンなグラフ作成、PHP mySQLとの組み合わせ</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet"><!--　←　触らないBootstrap v4.3.1　-->
-    <script src="js/jquery-3.4.1.min.js"></script><!--　←　触らない　-->
-    <script src="js/bootstrap.min.js"></script><!--　←　触らない　-->
-    <script src="js/Chart.min.js"></script>    
-    <style>
-        h1{font-size:1.3rem;}
-        .table-font { font-size:0.7rem; }
-        @media screen and (max-width: 480px) { 
-        	.sm-non { display:none; }
-        } 
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-<div class="container">	
-	<div style="">
-		<div style="width:120px;height:auto;"><a href="https://chikaraemon.com/"><img src="logo.jpg"></a></div>
-	</div>
-    <h1 class="text-primary my-3">いろんな人々の面白要素をレーダーチャートで表現。探してみよう、あなたも求めるユーモアを。 </h1>
-	<div class="row">
-		<div class="col-md-5">
-            <form method="POST" anction="<?= $_SERVER['SCRIPT_NAME'] ?>" class="my-3">
-                <select name="shurui" onchange="submit(this.form)">
-                    <option value="radar"<?php if( $shurui=="radar" ){ echo "selected"; } ?>>レーダーチャート</option>
-                    <option value="line"<?php if( $shurui=="line" ){ echo "selected"; } ?>>線グラフ</option>
-                    <option value="bar"<?php if( $shurui=="bar" ){ echo "selected"; } ?>>棒グラフ</option>
-                </select>
-                <input type="submit" value="グラフ種類を変える" class="btn btn-primary">
-            </form>
- 
-            <div class="text-sm">※面白要素は独断と偏見で設定しております。修正する場合も大いにあります。</div>
-            <div class="text-sm"><BR>ご容赦ください。</div>
-		</div>
-		<div class="col-md-7">	
-            <!--以下　グラフ-->
-            <canvas id="myChart"></canvas>
-            <script>
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: '<?= $shurui ?>',
-                data: {
-                    labels: ['カルシウム', '鉄', 'ビタミンA', 'ビタミンB1', 'ビタミンB2', 'ナイアシン', 'ビタミンC', 'ビタミンD'],
-                    datasets: [<?= $data ?>]
-                }
-            });
-            </script>
-            <!--以上　グラフ-->
-		</div>
-	</div>
-	
-</div>
+
+
+
+
+
+    <canvas id="myChart" width="400" height="400"></canvas>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"></script>
+    <script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'ほげ'],//各棒の名前（name)
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 20],//各縦棒の高さ(値段)
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    </script>
 </body>
 </html>

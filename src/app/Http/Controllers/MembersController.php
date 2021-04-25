85c2fa2528008ca0f4fd2b85607427a7eb0c7267
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-04-25 17:46:35 +0900

    modify

--- src/app/Http/Controllers/MembersController.php
+++ src/app/Http/Controllers/MembersController.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Humor;
 
 class MembersController extends Controller
 {
-    public function index(){
-        $members = Humor::orderBy('id','asc')->get();
-        return view('index',['members' => $members]);
+    public function show(Request $request){
+        $members = Humor::get();
+    
+        return view('humor_chart', ['members' => $members]);
     }
 
 }
